c671af5892226b2e3d04f11ad01f137244875972
shama2025 <2003mashaffer@gmail.com>
2024-09-10 23:05:11 -0400

    Update landing page.ts

--- professional-site/src/app/landing-page/landing-page.component.ts
+++ professional-site/src/app/landing-page/landing-page.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
-
+import { LandingPageServiceService } from '../service/landing-page-service.service';
+import { WORKEXP } from '../professional-interfaces';
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent {
-  constructor(){}
+  constructor(private landingPageService: LandingPageServiceService){}
 
 
   ngOnInit(): void {
-
+    let interfaces = this.landingPageService.getInterfaces()
   }
 
 }
--- professional-site/src/app/service/landing-page-service.service.ts
+++ professional-site/src/app/service/landing-page-service.service.ts
@@ -9,4 +9,8 @@ import { Injectable } from '@angular/core';
 export class LandingPageServiceService {
 
   constructor() { }
+
+  getInterfaces(){
+    return [WORKEXP,PROJECTS,LINKS,]
+  }
 }
